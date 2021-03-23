14d533b795a32d89d7d9de508e7155bb8458271e
Eric <planetside0@gmail.com>
2021-03-23 19:57:20 -0400

    updating the initdb file

--- initdb.py
+++ initdb.py
@@ -1,4 +1,12 @@
+import os
+import re
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 from app import db
+
 
 # db.drop_all()
 db.create_all()
